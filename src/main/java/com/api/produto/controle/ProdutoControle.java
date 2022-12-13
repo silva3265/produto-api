6e3ddc57bbf4a537849b53ab4fc1816c03777450
Rômulo <silva3265@gmail.com>
2022-12-13 20:19:29 -0300

    Deletar dados com o comando delete()

--- src/main/java/com/api/produto/controle/ProdutoControle.java
+++ src/main/java/com/api/produto/controle/ProdutoControle.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.produto.modelo.ProdutoModelo;
+import com.api.produto.modelo.RespostaModelo;
 import com.api.produto.repositorio.ProdutoRepositorio;
 
 @RestController
@@ -21,13 +22,13 @@ public class ProdutoControle {
 	@Autowired
 	private ProdutoRepositorio acoes; //pra dizer pro Spring que esse "acoes" é um repositorio a gente coloca a anotação @Autowired
 	
-	// Listar produtos
+	// Listar Produtos
 	@RequestMapping(value="/produtos", method=RequestMethod.GET) //aqui temos um controle "valor /produtos"
 	public @ResponseBody List<ProdutoModelo> listar() {
 		return acoes.findAll(); //o retorno vai ser do tipo List ProdutoModeo, que no caso é o findAll
 	}
 	
-	// Cadastrar produtos
+	// Cadastrar Produtos
 	@RequestMapping(value="/produtos", method=RequestMethod.POST) //ele vai executar essa posição de cadastro
 	public @ResponseBody ProdutoModelo cadastrar(@RequestBody ProdutoModelo produto) {
 		return acoes.save(produto); //o save pede um ProdMod do tipo produto
@@ -40,14 +41,32 @@ public class ProdutoControle {
 		return acoes.findByCodigo(codigo);
 	}
 	
-	// Alterar produto
+	// Alterar Produtos
 	// o save pode ser utilizado para cadastro ou para alteração, a unica coisa que muda é a requisição (POST para PUT)
 	@RequestMapping(value="/produtos", method=RequestMethod.PUT) //
 	public @ResponseBody ProdutoModelo alterar(@RequestBody ProdutoModelo produto) {
 		return acoes.save(produto);
 	}
 	
-	
+	//Remover Produtos
+	//Primeiro vamos pegar o codigo, depois tranformar esse codigo no nosso produto e em seguida vamos fazer a exclusao
+	@RequestMapping(value="/produtos/{codigo}", method=RequestMethod.DELETE)
+	public @ResponseBody RespostaModelo remover(@PathVariable Integer codigo) {
+		
+		RespostaModelo resposta = new RespostaModelo();
+		
+		try {
+			ProdutoModelo produto = filtrar(codigo);
+			this.acoes.delete(produto);
+			resposta.setMensagem("Produto removido com sucesso");
+		}catch(Exception erro) {
+			resposta.setMensagem("Falaha ao remover: " + erro.getMessage());
+			}
+		 
+		return resposta;
+		
+	}
+
 }
 
 
---
+++ src/main/java/com/api/produto/modelo/RespostaModelo.java
@@ -0,0 +1,18 @@
+package com.api.produto.modelo;
+
+public class RespostaModelo {
+	
+	//Atributo
+	private String mensagem;
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	
+
+}
