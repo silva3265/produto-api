508ff2915e93acbd3c67d547b16a3482eeff0925
Rômulo <silva3265@gmail.com>
2022-12-13 18:46:51 -0300

    Cadastrar dados com o comando save()

--- src/main/java/com/api/produto/controle/ProdutoControle.java
+++ src/main/java/com/api/produto/controle/ProdutoControle.java
@@ -3,6 +3,7 @@ package com.api.produto.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,6 +26,11 @@ public class ProdutoControle {
 		return acoes.findAll(); //o retorno vai ser do tipo List ProdutoModeo, que no caso é o findAll
 	}
 	
+	// Cadastrar produtos
+	@RequestMapping(value="/produtos", method=RequestMethod.POST) //ele vai executar essa posição de cadastro
+	public @ResponseBody ProdutoModelo cadastrar(@RequestBody ProdutoModelo produto) {
+		return acoes.save(produto); //o save pede um ProdMod do tipo produto
+	}
 	
 
 }
